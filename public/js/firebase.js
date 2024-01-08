74eec6ecbf070dabdbc57652167ab2481707149d
HarryG <Mangogrice08@gmail.com>
2024-01-09 00:49:21 +1300

    Update js/firebase.js

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,5 +1,11 @@
 let firebaseConfig = {
-    // Enter your firebase credentials
+    apiKey: "AIzaSyChowsfulXM1dXnOqqJOc4Q0Yj6n37BxKU",
+    authDomain: "fitsynblog.firebaseapp.com",
+    projectId: "fitsynblog",
+    storageBucket: "fitsynblog.appspot.com",
+    messagingSenderId: "82997781447",
+    appId: "1:82997781447:web:54690df9cd86ff0d6e6fe2",
+    measurementId: "G-SMB0ZHGZ2P"
 };
 
 firebase.initializeApp(firebaseConfig);
